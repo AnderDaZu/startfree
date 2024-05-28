a76d0eadf85de72fbb01c671b2c88fb9a4d2dca9
anderDaZu <anderda-96@hotmail.com>
2024-05-27 22:01:01 -0500

    Instalación y configuración de Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <div class="flex flex-col items-center"> 
-    <img class="max-w-xs" alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <CounterComponent/>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import CounterComponent from './components/CounterComponent'
-
-export default {
-  name: 'App',
-  components: {
-    // HelloWorld, 
-    CounterComponent
-  }
-}
-</script>
 
 <style>
 #app {
@@ -26,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
