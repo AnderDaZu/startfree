a5af324cf29ab2b4e35e71f35e2dbc6a2f393bfc
anderDaZu <anderda-96@hotmail.com>
2024-05-30 15:34:36 -0500

    Ciclos de vida de Vue Router

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -4,7 +4,30 @@
 
 <script>
 export default {
-
+    beforeCreate(){
+        console.log('before create from blog view');
+    },  
+    created() {
+        console.log('created from blog view')
+    },
+    beforeMount() {
+        console.log('before mount from blog view')
+    },
+    mounted() {
+        console.log('mounted from blog view')
+    },
+    beforeUpdate() {
+        console.log('before update from blog view');
+    },
+    updated() {
+        console.log('updated from blog view')
+    },
+    beforeUnmount() {
+        console.log('before unmount from blog view')
+    },
+    unmounted() {
+        console.log('Unmounted from blog view')
+    }
 }
 </script>
 
